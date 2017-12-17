977dd8513caaec0009198a5ff60beecc972da57b
John Schoeman <johnschoeman1617@gmail.com>
2017-12-17 10:24:20 -0500

    Fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://whispering-anchorage-75077.herokuapp.com/cabel'
+  config.action_cable.url = 'wss://whispering-anchorage-75077.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = [ 'https://whispering-anchorage-75077.herokuapp.com/', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
