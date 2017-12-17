fbcc91e86b5752906989f84527bc035bafc1bd5a
John Schoeman <johnschoeman1617@gmail.com>
2017-12-17 10:28:00 -0500

    Prep for launch 2

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'jquery-rails',            '4.1.1'
 gem 'turbolinks',              '5.0.0'
 gem 'jbuilder',                '2.4.1'
 gem 'kramdown'
-gem 'redis'
+gem 'redis',                   '3.3.1'
 
 group :development, :test do
   gem 'sqlite3', '1.3.13'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   config.action_cable.url = 'wss://whispering-anchorage-75077.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://whispering-anchorage-75077.herokuapp.com/', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ 'https://whispering-anchorage-75077.herokuapp.com' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
